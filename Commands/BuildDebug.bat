5c8fb309385f8a2ea89c31c9194d02005ccad812
briandl2000 <brian@delangecastricum.nl>
2025-09-17 20:44:45 +0200

    removed uneeded files

--- Commands/BuildDebug.bat
+++
@@ -1,7 +0,0 @@
-cd ..
-mkdir build
-cd build
-cmake .. -DRELEASE=OFF
-cmake --build . --config Debug
-
-ECHO "Finished building Debug"
--- Commands/BuildRelease.bat
+++
@@ -1,7 +0,0 @@
-cd ..
-mkdir build
-cd build
-cmake .. -DRELEASE=ON
-cmake --build . --config Release
-
-ECHO "Finished building Release"
