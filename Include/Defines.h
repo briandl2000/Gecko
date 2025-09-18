4c691691cec98db284f64c45b7943bc9625c6f2b
briandl2000 <brian@delangecastricum.nl>
2025-09-18 22:00:20 +0200

    prepare restructoring of the project

--- Include/Defines.h
+++ Include/Defines.h
@@ -19,7 +19,7 @@ namespace Gecko
 using u8 = uint8_t;
 using u16 = uint16_t;
 using u32 = uint32_t;
-using u64 = uint64_t ;
+using u64 = uint64_t;
 
 using i8 = int8_t;
 using i16 = int16_t;
@@ -27,7 +27,7 @@ using i32 = int32_t;
 using i64 = int64_t;
 
 using f32 = float;
-using f64 = double ;
+using f64 = double;
 
 #define STATIC_ASSERT static_assert
 
