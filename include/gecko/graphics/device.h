89b7a2c2a3cfab0ba516c57ace6ed79c26c9f326
Brian de Lange <brian@delangecastricum.nl>
2026-04-21 21:07:46 +0200

    chore(graphics): remove unused device.h redirect header

--- include/gecko/graphics/device.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// GraphicsDevice (the abstract backend class) lives in graphics_device.h.
-// This header is kept for convenience / backward compatibility.
-#include "gecko/graphics/graphics_device.h"
