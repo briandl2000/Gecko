9253134f815c0718e17a662cd544c42e7eb88124
Brian de Lange <brian@delangecastricum.nl>
2026-07-19 10:07:43 +0200

    fix: link the Windows shell API

--- build.bat
+++ build.bat
@@ -57,7 +57,7 @@ echo [CXX  ] Gecko.dll
 cl %CommonFlags% /MP /LD %ConfigFlags% /DGECKO_BUILDING=1 /I"%Root%include" ^
  /I"%Root%src\core" /I"%Root%src\graphics" %VulkanInclude% ^
  "%Root%src\gecko_engine.cpp" /Fo"%ObjectDir%\gecko_engine.obj" /Fe"%BinaryDir%\Gecko.dll" ^
- /link /IMPLIB:"%BinaryDir%\Gecko.lib" user32.lib shcore.lib ole32.lib winmm.lib %VulkanLibrary%
+ /link /IMPLIB:"%BinaryDir%\Gecko.lib" user32.lib shell32.lib shcore.lib ole32.lib winmm.lib %VulkanLibrary%
 if errorlevel 1 exit /b 1
 
 if /I "%Action%"=="examples" (
